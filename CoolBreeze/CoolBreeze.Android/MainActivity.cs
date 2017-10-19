0f4cf1f39139e3b7f82db41a3e1f9344f40874de
Aiko Sherina N. Regalo <Aiko.Regalo@tmp.local>
2017-10-19 10:17:49 +0800

    Add project files.

---
+++ CoolBreeze/CoolBreeze.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CoolBreeze.Droid
+{
+    [Activity(Label = "CoolBreeze", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Microsoft.Azure.Mobile.MobileCenter.Configure("f79d1910-7e0e-4f12-9d42-78c2ede25d74");
+            LoadApplication(new App());
+        }
+    }
+}
+
